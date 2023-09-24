78cda4c67ae9282b0a391fd1c55e4767582ab60d
Mircea Pavileanu <mircea.pavileanu@solera.com>
2023-09-25 00:53:18 +0300

    Add project files.

---
+++ Redis/Program.cs
@@ -0,0 +1,17 @@
+﻿using StackExchange.Redis;
+
+string connectionString = "";
+
+using (var cache = ConnectionMultiplexer.Connect(connectionString))
+{
+    IDatabase db = cache.GetDatabase();
+
+    var result = await db.ExecuteAsync("ping");
+    Console.WriteLine($"PING = {result.Type} : {result}");
+
+    bool setValue = await db.StringSetAsync("keys:mircea", "100");
+    Console.WriteLine($"SET: {setValue}");
+
+    string getValue = await db.StringGetAsync("keys:mircea");
+    Console.WriteLine($"GET: {getValue}");
+}
